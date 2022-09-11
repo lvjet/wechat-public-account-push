96a1d24b99321ebbac80539dc1da279945cebdbe
lvjet <81807419+lvjet@users.noreply.github.com>
2022-09-11 23:44:44 +0800

    Update index.js

--- config/index.js
+++ config/index.js
@@ -68,6 +68,25 @@ export const config = {
       openUrl: "https://wangxinleo.cn",
       festivals: [],
       customizedDateList: []
+      // 所在省份
+      province: "江苏",
+      // 所在城市
+      city: "扬州",
+      // 新历生日, 仅用作获取星座运势, 格式必须
+      horoscopeDate: '09-18',
+      // 获取什么时候的星座运势，可选：['今日', '明日', '本周', '本月', '今年'], 留空则随机
+      horoscopeDateType: '今日',
+      // 他点击详情后跳转的页面,你可以设置成微博的热榜，也可以设置成其他，网址一定要填对；不填对也没关系，随便你，会打不开而已。
+      openUrl: "https://wangxinleo.cn",
+      // 专属节日提醒，如果你在这里填写了节日提醒，就不会执行FESTIVALS的提醒了, 和FESTIVALS的配置方法相同，可以往下查看，我这里就不重复写了
+      festivals: [
+        // 注意：此条配置日期为阴历日期，因为`type`中 “生日” 之前有 * 符号
+        {"type": "*生日", "name": "宝宝", "year": "1997", "date": "08-17"},
+        {"type": "节日", "name": "恋爱纪念日", "year": "2022", "date": "06-10"},
+        // 注意：此条配置日期为阳历日期，因为`type`中 “生日” 之前没有 * 符号
+        //{"type": "生日", "name": "宝宝", "year": "1997", "date": "09-18"},
+        //{"type": "节日", "name": "恋爱纪念日", "year": "2022", "date": "06-10"},
+      ],
     },
     
     // 你可以不断按格式往下增加
